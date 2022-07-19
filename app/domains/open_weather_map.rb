d5378faaa26309f121a231c512e3bc525db37c6d
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-07-20 00:58:37 +0200

    Clean trivial files

--- app/domains/open_weather_map.rb
+++ app/domains/open_weather_map.rb
@@ -16,10 +16,10 @@ module OpenWeatherMap
     ids_arr = items.map { |name| OpenWeatherMap::Resolver.city_id(name) }
     ids_arr.compact!
     ids_arr = ids_arr.join(',')
-    
+
     response = HTTP.get("https://api.openweathermap.org/data/2.5/group?id=#{ids_arr}&appid=37232143b89be9c7b2e089648313c3b9")
     json_cities = JSON.parse(response.body.to_s)
-    
+
     json_cities['list'].map { |city| OpenWeatherMap::City.parse(city) }
   end
 end
