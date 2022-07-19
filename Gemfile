2767262294e37aa263b5e36fa25ca4a39b3836a0
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-07-19 12:53:46 +0200

    Add resolver module and resolver specs

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'puma', '~> 5.0'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+gem "http"
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
---
+++ app/domains/open_weather_map/resolver.rb
@@ -0,0 +1,11 @@
+require 'json'
+
+module OpenWeatherMap
+  module Resolver
+    def self.city_id(city_name)
+      cities = JSON.parse(File.read(File.expand_path('small.json', __dir__)))
+      city = cities.find { |c| c['name'] == city_name }
+      city.nil? ? city : city['id']
+    end
+  end
+end
