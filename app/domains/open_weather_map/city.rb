ebc672d13c64fbb42d45130402c4098c9c806843
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-07-22 13:38:37 +0200

    Fix rubocop

--- app/domains/open_weather_map/city.rb
+++ app/domains/open_weather_map/city.rb
@@ -1,31 +1,33 @@
-class OpenWeatherMap::City
-  include Comparable
-  attr_reader :id, :lat, :lon, :name
-  attr_accessor :temp_k # TODO: change spec
+module OpenWeatherMap
+  class City
+    include Comparable
+    attr_reader :id, :lat, :lon, :name
+    attr_accessor :temp_k # TODO: change spec
 
-  def initialize(id:, lat:, lon:, name:, temp_k:)
-    @id = id
-    @lat = lat
-    @lon = lon
-    @name = name
-    @temp_k = temp_k
-  end
+    def initialize(id:, lat:, lon:, name:, temp_k:)
+      @id = id
+      @lat = lat
+      @lon = lon
+      @name = name
+      @temp_k = temp_k
+    end
 
-  def self.parse(raw_hash)
-    hash = raw_hash.deep_symbolize_keys
+    def self.parse(raw_hash)
+      hash = raw_hash.deep_symbolize_keys
 
-    new(
-      id: hash[:id], name: hash[:name],
-      lat: hash.dig(:coord, :lat), lon: hash.dig(:coord, :lon),
-      temp_k: hash.dig(:main, :temp)
-    )
-  end
+      new(
+        id: hash[:id], name: hash[:name],
+        lat: hash.dig(:coord, :lat), lon: hash.dig(:coord, :lon),
+        temp_k: hash.dig(:main, :temp)
+      )
+    end
 
-  def temp
-    (temp_k - 273.15).round(2)
-  end
+    def temp
+      (temp_k - 273.15).round(2)
+    end
 
-  def <=>(other)
-    [temp, name.ord] <=> [other.temp, other.name.ord]
+    def <=>(other)
+      [temp, name.ord] <=> [other.temp, other.name.ord]
+    end
   end
 end
--- app/domains/open_weather_map/resolver.rb
+++ app/domains/open_weather_map/resolver.rb
@@ -1,6 +1,14 @@
-module OpenWeatherMap::Resolver
-  def self.city_id(name)
-    city_id = JSON.parse(File.read(File.expand_path('city_ids.json', __dir__))).to_h { |city| [city['name'], city['id']] }[name.to_s] # rubocop:disable Layout/LineLength
-    city_id.nil? ? nil : city_id
+module OpenWeatherMap
+  module Resolver
+    def self.city_id(name)
+      city_id = JSON.parse(
+        File.read(
+          File.expand_path('city_ids.json', __dir__)
+        )
+      ).to_h do |city|
+        [city['name'], city['id']][name.to_s]
+      end
+      city_id.nil? ? nil : city_id
+    end
   end
 end
