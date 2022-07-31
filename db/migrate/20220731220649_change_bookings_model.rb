944d57a3521d21042a2e0321b4e297ff3215b673
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-08-01 00:24:49 +0200

    Return default bookings controller create action

--- db/migrate/20220731220649_change_bookings_model.rb
+++
@@ -1,6 +0,0 @@
-class ChangeBookingsModel < ActiveRecord::Migration[6.1]
-  def change
-    change_column_null :bookings, :user_id, true
-    change_column_null :bookings, :flight_id, true
-  end
-end
