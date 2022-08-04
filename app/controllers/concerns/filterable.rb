9e617f677711bba3192764f6d185d37f06121787
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-08-05 01:02:39 +0200

    Test filterable module in flight controller

--- app/controllers/concerns/filterable.rb
+++ app/controllers/concerns/filterable.rb
@@ -5,7 +5,7 @@ module Filterable
     def filter(filtering_params)
       results = where(nil)
       filtering_params.each do |key, value|
-        raise StandardError, 'This is an exception' if value.blank? # rubocop:disable Style/RaiseArgs
+        raise StandardError, 'This is an exception' if value.blank?
 
         results = results.public_send("filter_by_#{key}", value) if value.present?
       end
