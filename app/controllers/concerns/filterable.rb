d67c8b18ece936f6b6e79ab51b216601f42f3a1a
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-08-05 00:55:59 +0200

    Test filterable module in flight controller

--- app/controllers/concerns/filterable.rb
+++ app/controllers/concerns/filterable.rb
@@ -3,10 +3,13 @@ module Filterable
 
   module ClassMethods
     def filter(filtering_params)
+      #binding.pry
       results = where(nil)
       filtering_params.each do |key, value|
+        raise StandardError.new "This is an exception" unless value.present?
         results = results.public_send("filter_by_#{key}", value) if value.present?
       end
+      #binding.pry
       results
     end
   end
