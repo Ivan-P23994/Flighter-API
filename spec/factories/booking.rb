3cde2bc5afd3f0a71bfaeba02d3eb39245788461
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-08-04 23:01:10 +0200

    Fix booking seats validation

--- spec/factories/booking.rb
+++ spec/factories/booking.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :booking do
-    no_of_seats { rand(1..80) }
+    no_of_seats { rand(1..20) }
     seat_price { rand(50..1000) }
     user { association :user }
     flight { association :flight }
--- spec/factories/flight.rb
+++ spec/factories/flight.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     sequence(:name) { |n| "flight-#{n}" }
     departs_at { DateTime.now + rand(1..99).years + rand(1..99).days }
     arrives_at { departs_at + 2.days }
-    no_of_seats { rand(80..250) }
+    no_of_seats { rand(350..2250) }
     base_price { rand(1..150) }
     company { association :company }
   end
