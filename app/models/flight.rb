5b1e116da4ed6ddfccc542621823d85e395ec278
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-08-07 20:05:07 +0200

    Apply patch

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -34,7 +34,8 @@ class Flight < ApplicationRecord
   scope :filter_by_departs_at_eq, ->(time) { where("date_trunc('minute', departs_at) = ?", time.slice(0..15)) } # rubocop:disable Layout/LineLength
   scope :filter_by_no_of_available_seats_qteq,
         lambda { |seats|
-          left_joins(:bookings)
+          active_flights
+            .left_joins(:bookings)
             .group(:id)
             .having('flights.no_of_seats - COALESCE(SUM(bookings.no_of_seats), flights.no_of_seats) >=  ?', seats)   # rubocop:disable Layout/LineLength
         }
